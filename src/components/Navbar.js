8a4196d4c02bcb8a0f314130625c44bd5b9a4e2e
vinayak180203 <vinayakk@iitbhilai.ac.in>
2024-07-11 11:32:35 +0530

    Changed logo and Navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
+import logo from "../Assets/ACCL_small.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/vinayak180203/ACCL"
                 target="_blank"
                 className="fork-btn-inner"
               >
