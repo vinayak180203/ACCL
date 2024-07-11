dc2102d418a344d88b74ac7038ab75fc52684ef5
vinayak180203 <vinayakk@iitbhilai.ac.in>
2024-07-11 11:37:00 +0530

    Fixed Image CSS

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -30,12 +30,12 @@ function Home() {
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
+            <Col md={5} style={{ paddingBottom: 20, margin: "auto 0" }}>
               <img
                 src={homeLogo}
                 alt="home pic"
                 className="img-fluid"
-                style={{ maxHeight: "450px" }}
+                style={{ maxHeight: "450px", transform: "scale(1.5)" }}
               />
             </Col>
           </Row>
